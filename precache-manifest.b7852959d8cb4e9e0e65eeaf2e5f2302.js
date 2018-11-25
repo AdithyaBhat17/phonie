5ec8faee0a70f100b24405299c0a8ca5faab075f
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-11-26 01:43:22 +0530

    Updates

---
+++ precache-manifest.b7852959d8cb4e9e0e65eeaf2e5f2302.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "be092dda3bea499c009b25a66aec65a3",
+    "url": "/phonie/static/media/unicorn.be092dda.svg"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/phonie/static/media/psr.eae9c18c.eae9c18c.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phonie/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "093cd7067b6f07f4b660270803f3763b",
+    "url": "/phonie/static/media/Kontanter-Bold.093cd706.otf"
+  },
+  {
+    "revision": "7d54c55044a73f528719",
+    "url": "/phonie/static/js/runtime~main.7d54c550.js"
+  },
+  {
+    "revision": "76406e6ef5ffcf9f79b0",
+    "url": "/phonie/static/js/main.76406e6e.chunk.js"
+  },
+  {
+    "revision": "b780aa09a477029d4f4f",
+    "url": "/phonie/static/js/1.b780aa09.chunk.js"
+  },
+  {
+    "revision": "76406e6ef5ffcf9f79b0",
+    "url": "/phonie/static/css/main.caedb49f.chunk.css"
+  },
+  {
+    "revision": "919f7f6226f4ced0e2591ad902b11114",
+    "url": "/phonie/index.html"
+  }
+];
